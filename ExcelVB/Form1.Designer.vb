80b0a57700f3248ce7388e6c5862658a92db8495
robbie <applefreak07@gmail.com>
2011-02-14 13:51:45 -0600

    Adding ExcelVB.sln and project files Demonstrates the basic use of the Excel COM library

---
+++ ExcelVB/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCreate = New System.Windows.Forms.Button
+        Me.txtInstructorName = New System.Windows.Forms.TextBox
+        Me.txtCRN = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.ListBox1 = New System.Windows.Forms.ListBox
+        Me.SuspendLayout()
+        '
+        'btnCreate
+        '
+        Me.btnCreate.Location = New System.Drawing.Point(197, 227)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCreate.TabIndex = 0
+        Me.btnCreate.Text = "Go"
+        Me.btnCreate.UseVisualStyleBackColor = True
+        '
+        'txtInstructorName
+        '
+        Me.txtInstructorName.Location = New System.Drawing.Point(101, 27)
+        Me.txtInstructorName.Name = "txtInstructorName"
+        Me.txtInstructorName.Size = New System.Drawing.Size(100, 20)
+        Me.txtInstructorName.TabIndex = 1
+        '
+        'txtCRN
+        '
+        Me.txtCRN.Location = New System.Drawing.Point(101, 54)
+        Me.txtCRN.Name = "txtCRN"
+        Me.txtCRN.Size = New System.Drawing.Size(100, 20)
+        Me.txtCRN.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Instructor Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(65, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(30, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "CRN"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(49, 99)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtCRN)
+        Me.Controls.Add(Me.txtInstructorName)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCreate As System.Windows.Forms.Button
+    Friend WithEvents txtInstructorName As System.Windows.Forms.TextBox
+    Friend WithEvents txtCRN As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+
+End Class
---
+++ ExcelVB/Form1.vb
@@ -0,0 +1,47 @@
+﻿Imports Microsoft.Office.Interop
+
+Public Class Form1
+    Dim app As Excel.Application
+    Dim book As Excel._Workbook
+    Dim count As Integer
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCreate.Click
+        'declare local variables
+        Dim books As Excel.Workbooks
+        Dim sheets As Excel.Sheets
+        Dim sheet As Excel._Worksheet
+        Dim range As Excel.Range
+
+        'start up excel, and create workspace
+        app = New Excel.Application
+        books = app.Workbooks
+        book = books.Add
+        sheets = book.Worksheets
+        sheet = sheets(1)
+
+        'work with the sheet  -- example 1
+        range = sheet.Range("A1:A3", Reflection.Missing.Value)
+        range.Interior.Color = RGB(40, 115, 40)
+        range.ColumnWidth = 20
+        sheet.Range("A1").Value = txtInstructorName.Text
+        sheet.Range("A2").Value = txtCRN.Text
+
+
+
+        'close excel when done
+
+        'app.Quit()
+        'Runtime.InteropServices.Marshal.ReleaseComObject(app)
+        'Runtime.InteropServices.Marshal.ReleaseComObject(books)
+        'app = Nothing
+        CloseExcel(app, books)
+    End Sub
+
+    Sub CloseExcel(ByRef application As Excel.Application, ByRef workbooks As Excel.Workbooks)
+        count += 1
+        txtCRN.Text = CStr(count)
+        application.Quit()
+        Runtime.InteropServices.Marshal.ReleaseComObject(workbooks)
+        Runtime.InteropServices.Marshal.ReleaseComObject(application)
+        application = Nothing
+    End Sub
+End Class
