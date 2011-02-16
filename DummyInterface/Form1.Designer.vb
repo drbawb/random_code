b33e95d87877f7aa2e088b6b64f4dd4a3b85e6a1
robbie <applefreak07@gmail.com>
2011-02-16 13:58:21 -0600

    "DummyInterface" added. Uses SQLServer Compact Edition + LINQ (via SQLMetal)

---
+++ DummyInterface/Form1.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainWindow
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CheckedListBox2 = New System.Windows.Forms.CheckedListBox
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.IDontKnowToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.WhatImDoingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ByWeekToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ByDayToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.SystemToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.LogoffToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.BackupToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.ListBox1 = New System.Windows.Forms.ListBox
+        Me.MenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'CheckedListBox2
+        '
+        Me.CheckedListBox2.FormattingEnabled = True
+        Me.CheckedListBox2.Items.AddRange(New Object() {"152-103, 12:30-3:00", "152-103, 3:25-5:00", "152-134, 8:00AM-3:30PM", "152-168, 7:25-9:00PM"})
+        Me.CheckedListBox2.Location = New System.Drawing.Point(499, 63)
+        Me.CheckedListBox2.Name = "CheckedListBox2"
+        Me.CheckedListBox2.Size = New System.Drawing.Size(153, 409)
+        Me.CheckedListBox2.TabIndex = 1
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.ViewToolStripMenuItem, Me.SystemToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(884, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.IDontKnowToolStripMenuItem, Me.WhatImDoingToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'IDontKnowToolStripMenuItem
+        '
+        Me.IDontKnowToolStripMenuItem.Name = "IDontKnowToolStripMenuItem"
+        Me.IDontKnowToolStripMenuItem.Size = New System.Drawing.Size(157, 22)
+        Me.IDontKnowToolStripMenuItem.Text = "I don't know"
+        '
+        'WhatImDoingToolStripMenuItem
+        '
+        Me.WhatImDoingToolStripMenuItem.Name = "WhatImDoingToolStripMenuItem"
+        Me.WhatImDoingToolStripMenuItem.Size = New System.Drawing.Size(157, 22)
+        Me.WhatImDoingToolStripMenuItem.Text = "What I'm Doing"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'ViewToolStripMenuItem
+        '
+        Me.ViewToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ByWeekToolStripMenuItem, Me.ByDayToolStripMenuItem})
+        Me.ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
+        Me.ViewToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.ViewToolStripMenuItem.Text = "View"
+        '
+        'ByWeekToolStripMenuItem
+        '
+        Me.ByWeekToolStripMenuItem.Name = "ByWeekToolStripMenuItem"
+        Me.ByWeekToolStripMenuItem.Size = New System.Drawing.Size(119, 22)
+        Me.ByWeekToolStripMenuItem.Text = "By Week"
+        '
+        'ByDayToolStripMenuItem
+        '
+        Me.ByDayToolStripMenuItem.Name = "ByDayToolStripMenuItem"
+        Me.ByDayToolStripMenuItem.Size = New System.Drawing.Size(119, 22)
+        Me.ByDayToolStripMenuItem.Text = "By Day"
+        '
+        'SystemToolStripMenuItem
+        '
+        Me.SystemToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LogoffToolStripMenuItem, Me.BackupToolStripMenuItem})
+        Me.SystemToolStripMenuItem.Name = "SystemToolStripMenuItem"
+        Me.SystemToolStripMenuItem.Size = New System.Drawing.Size(57, 20)
+        Me.SystemToolStripMenuItem.Text = "System"
+        '
+        'LogoffToolStripMenuItem
+        '
+        Me.LogoffToolStripMenuItem.Name = "LogoffToolStripMenuItem"
+        Me.LogoffToolStripMenuItem.Size = New System.Drawing.Size(113, 22)
+        Me.LogoffToolStripMenuItem.Text = "Logoff"
+        '
+        'BackupToolStripMenuItem
+        '
+        Me.BackupToolStripMenuItem.Name = "BackupToolStripMenuItem"
+        Me.BackupToolStripMenuItem.Size = New System.Drawing.Size(113, 22)
+        Me.BackupToolStripMenuItem.Text = "Backup"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 485)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(884, 22)
+        Me.StatusStrip1.TabIndex = 3
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(140, 17)
+        Me.ToolStripStatusLabel1.Text = "Created by codeBUSTERS"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 27)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(481, 455)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Schedule Viewer"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Maroon
+        Me.Label3.Location = New System.Drawing.Point(9, 58)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(246, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "This violates busines rule: ""10 HR allocation span"""
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.Maroon
+        Me.Label2.Location = New System.Drawing.Point(9, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "WARNING!"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Maroon
+        Me.Label1.Location = New System.Drawing.Point(9, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(469, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Resource ""Matt"" has been assigned to ""152-103 8:00AM - 3:30PM"" and ""152-168, 7:25" & _
+            "-9:00PM"""
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Eggbert", "Matt", "Melissa", "Jeff"})
+        Me.ComboBox1.Location = New System.Drawing.Point(499, 36)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(152, 21)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 150)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(97, 230)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 4
+        '
+        'MainWindow
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(884, 507)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.CheckedListBox2)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "MainWindow"
+        Me.Text = "D.A.R.P.A - Dynamic Application for Resource and People Allocation"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents CheckedListBox2 As System.Windows.Forms.CheckedListBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents IDontKnowToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents WhatImDoingToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ViewToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ByWeekToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ByDayToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SystemToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LogoffToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BackupToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+
+End Class
---
+++ DummyInterface/Form1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data.Linq
+Public Class MainWindow
+
+    Private Sub GroupBox1_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub MainWindow_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim db As New DataContext("Northwind.sdf")
+        Dim customers As Table(Of Customers) = db.GetTable(Of Customers)()
+
+        For Each cust As Customers In customers
+            ListBox1.Items.Add(cust.ContactName)
+        Next
+
+    End Sub
+End Class
